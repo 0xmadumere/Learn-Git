0d027e50460557b4312681a4b8e4466edb7f2e0a
rishsz <anthonyobinnaorji@gmail.com>
2026-07-29 13:01:56 +0100

    omo

---
+++ classwork2.js
@@ -0,0 +1,16 @@
+let score = 80;
+
+switch (score)
+{
+case 90:
+    console.log("A");
+    break;
+case 80:
+    console.log("B");
+    break;
+case 70:
+    console.log("C");
+    break;
+default:
+    console.log("F");
+}
